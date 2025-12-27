e2c489a1ca7eb8ac28a9f24c99bdaf61234de577
DenisEvd <den-evdokimov-2015@mail.ru>
2025-12-28 00:45:41 +0300

    ref: adjust params

--- src/main/kotlin/ru/quipy/payments/client/ExternalPaymentClient.kt
+++ src/main/kotlin/ru/quipy/payments/client/ExternalPaymentClient.kt
@@ -48,7 +48,7 @@ class ExternalPaymentClient(
 
     private suspend fun executeWithParallelLimitCheck(request: HttpRequest, deadline: Instant): ExternalSysResponse? {
         if (!outboundParallelRequestLimiter.tryToAddRequest(10)) {
-            throw ResourceExhaustedRetryableException(100)
+            throw ResourceExhaustedRetryableException(1000)
         }
 
         try {
@@ -62,7 +62,7 @@ class ExternalPaymentClient(
         try {
             RateLimiter.waitForPermission(outboundRateLimiter)
         } catch (e: Exception) {
-            throw ResourceExhaustedRetryableException(100)
+            throw ResourceExhaustedRetryableException(1000)
         }
 
         val response = client.sendAsync(request, HttpResponse.BodyHandlers.ofString()).await()
